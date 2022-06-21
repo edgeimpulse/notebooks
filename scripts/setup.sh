7728045cf41f1decbe0074c5b782a56a0986747e
Jorge Silva <jorge@edgeimpulse.com>
2022-06-21 16:32:18 -0400

    Also install dev dependencies in virtual env.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,12 @@ set -e # Exit on error
 
 # We assume we are in python 3
 pip install pipenv
-pipenv install
+
+# Tell pipenv to install all packages including dev dependencies
+pipenv install --dev
+# Tell pipenv to install all packages including dev dependencies
+# and local directory as editable
+#pipenv install --dev -e .
+
+# Install the pre-commit hook
 pipenv run pre-commit install
