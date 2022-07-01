9bbd42e7cd4df18891f1e5d177c85c36928a67d4
Jorge Silva <jorge@edgeimpulse.com>
2022-07-01 03:33:58 -0400

    Fix path to notebooks

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e # Exit on error
 
+cd notebooks
 pipenv run jupyter nbconvert --clear-output --inplace notebooks.ipynb
-# Check for changes (though ignore mode changes)
+# Check for changes (ignore mode changes)
 if [ -z "$(git -c core.fileMode=false status --porcelain)" ]; then
   echo "Repository is clean."
 else
